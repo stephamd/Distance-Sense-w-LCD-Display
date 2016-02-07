ca51be9416ebcc477b2aae19bf8331aa8ee842ab
stephamd <stephamd@mail.uc.edu>
2016-02-06 19:28:58 -0500

    Update DistanceSense.py

--- DistanceSense.py
+++ DistanceSense.py
@@ -1,1 +1,35 @@
 
+#!/usr/bin/python
+#
+# Distance Sense with LCD Display realtime
+# Raspberry Pi
+#
+# Author : Matt Stephan
+# site: github.com/stephamd
+# Date   : 02/06/2016
+#
+
+import HC-SensorDriver as sensor
+import LCD_Driver as LCD
+
+
+
+def main():
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+  
+  
+  
+if __name__ == '__main__':
+  main()
