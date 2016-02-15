12eb216a48f207db45aaec0faf2a002053846933
stephamd <stephamd@mail.uc.edu>
2016-02-15 14:38:56 -0500

    Update DistanceSense.py
    Updated from branch

--- DistanceSense.py
+++ DistanceSense.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 #
 # Distance Sense with LCD Display realtime
@@ -13,45 +12,59 @@ import RPi.GPIO as GPIO
 import time
 
 import HC_SR04_Driver
-#import  LCD_Driver
+import  LCD_Driver
 
 
 
 def main():
-  	
 
 
-	#GPIO DEBUG:Scanning for a 1
 
-	#gpioScan(6,1)
-	
-	#print "\033[2J"
-	print "Distance Sensor"
-	print "*********************************"
+        #GPIO DEBUG:Scanning for a 1
 
-	distSensor = HC_SR04_Driver.HCSensorModule(19,6)
+        #gpioScan(6,1)
 
-  	sensorTimeStart = time.time()
-  	
-  	while(deltaT(sensorTimeStart) < 10):
-		distSensor.measure()
-		time.sleep(.2)
-	
-	
-    	print "Done measuring. Measure time" , deltaT(sensorTimeStart)
+        #print "\033[2J"
+        print "Distance Sensor"
+        print "*********************************"
 
-    	GPIO.cleanup()
+        distSensor = HC_SR04_Driver.HCSensorModule(19,6)
+
+        lcd = LCD_Driver.LCD_Driver(25,24,23,17,21,22)
+
+        lcd.updateLine(1,"Distance Sense")
+        lcd.updateLine(2,"Matt S")
+        time.sleep(2)
+
+        lcd.updateLine(1,"Meas. (cm)")
+        lcd.updateLine(2,"")
+
+        sensorTimeStart = time.time()
+
+        try:
+                while True:
+                        meas = distSensor.measure()
+                        lcd.updateLine(2, str(meas))
+                        time.sleep(.25)
+
+        except KeyboardInterrupt:
+                pass
 
 
+        print "Done measuring. Measure time" , deltaT(sensorTimeStart)
+
+        GPIO.cleanup()
+        
 def deltaT(start):
-	deltaTime = time.time()-start
-	return deltaTime
+        deltaTime = time.time()-start
+        return deltaTime
 
 def gpioScan(pin,binary):
-	while True:
-		if GPIO.input(pin) == binary:
-			print "GPIO is 1 ", time.time()
-			return
+        while True:
+                if GPIO.input(pin) == binary:
+                        print "GPIO is 1 ", time.time()
+                        return
 
 if __name__ == '__main__':
   main()
+
